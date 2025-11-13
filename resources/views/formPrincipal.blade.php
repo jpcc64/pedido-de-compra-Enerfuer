9f43f0d71135056abaa161dbf0195dc3eb0a292b
Juan Pablo <jpcc2001@gmail.com>
2025-11-13 14:44:12 +0000

    cambio en el precio del producto

--- resources/views/formPrincipal.blade.php
+++ resources/views/formPrincipal.blade.php
@@ -114,7 +114,7 @@
                                             <input name="UnitPrice" type="number" min="0" id="UnitPrice"
                                                 class="form-input w-full min-w-0 resize-none overflow-hidden rounded-lg text-gray-800 dark:text-white focus:outline-0 focus:ring-2 focus:ring-primary/50 border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 focus:border-primary h-12 placeholder:text-gray-400 dark:placeholder-gray-500 p-3 text-base font-normal leading-normal"
                                                 placeholder="Precio en €"
-                                                value="{{ isset($productos['AvgStdPrice']) ? $productos['AvgStdPrice'] : '' }}" />
+                                                value="{{ isset($productos['AvgStdPrice']) ? ($productos['AvgStdPrice'] / 0.97) : '' }}" />
                                         </label>
                                     </div>
                                 </form>
@@ -182,25 +182,6 @@
                             class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 px-4 bg-primary text-white text-sm font-bold leading-normal tracking-[0.015em]">
                             <span class="truncate">Crear Pedido</span>
                         </button>
-                        <div id="spinner" role="status" hidden>
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white opacity-20">
-                                Noteworthy technology acquisitions 2021</h5>
-                            <p class="font-normal text-gray-700 dark:text-gray-400 opacity-20">Here are the biggest
-                                enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                            <div role="status" class="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2">
-                                <svg aria-hidden="true"
-                                    class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                                    viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                        fill="currentColor" />
-                                    <path
-                                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                        fill="currentFill" />
-                                </svg>
-                                <span class="sr-only">Loading...</span>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </main>
@@ -249,7 +230,7 @@
                         // Rellenar campos del producto
                         $itemNameInput.val(product.ItemName || '');
                         // Asigna AvgStdPrice como precio unitario si está disponible
-                        $unitPriceInput.val(parseFloat(product.AvgStdPrice || 0).toFixed(2));
+                        $unitPriceInput.val(parseFloat(product.AvgStdPrice/0.97 || 0).toFixed(2)); // se hace entre 0.97 para quitar el 3% de margen
                         $quantityInput.val('1'); // Pone 1 por defecto al buscar
                     } else {
                         alert('Producto no encontrado.');
@@ -449,12 +430,12 @@
                 }
             });
         $('#crearPedidoBtn').click(function (e) {
-           $.get('{{ route('pedidos.clearCarrito') }}')
-            .done(function (response) {
-                if (response.carrito) {
-                    updateCartTable(response.carrito);
-                }
-            });
+            $.get('{{ route('pedidos.clearCarrito') }}')
+                .done(function (response) {
+                    if (response.carrito) {
+                        updateCartTable(response.carrito);
+                    }
+                });
         });
 
     });
